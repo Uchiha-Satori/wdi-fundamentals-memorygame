a40f63352cd5d86b0ae9d75c9361bce3685e6c7a
Uchiha-Satori <stephen.kit.ho@gmail.com>
2017-07-18 17:24:12 -0400

    created game logic 2.0

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-console.log("Up and running!")
+console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
 }
-if (cardsInPlay[0] === cardsInPlay[2]) {
-  alert("You found a match!");
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId])
+  if (cardsInPlay.length === 2) {
+    checkForMatch()
 }
-else {
-  alert("Sorry, try again.")
 }
+flipCard(0);
+flipCard(2);
